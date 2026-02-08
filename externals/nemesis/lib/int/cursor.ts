c79493bc3de96eb9c8653add035c28350d222013
coderiaser <coderaiser@cloudcmd.io>
2026-02-08 23:39:14 +0200

    feature: nemesis: cursor: improve

--- externals/nemesis/lib/int/cursor.ts
+++ externals/nemesis/lib/int/cursor.ts
@@ -1,3 +1,4 @@
+import {io} from '@ishvara/operator-fasm';
 import {getLine, setLine} from './position/line.ts';
 import {getColumn, setColumn} from './position/column.ts';
 
@@ -27,28 +28,29 @@ export async function setCursor<es, dx>() {
     // select to write low byte of index
     al = 0xf;
     dx = 0x03d4;
-    out(dx, al);
+    io.out(dx, al);
     // write it
     al = bl;
     dx = 0x03d5;
-    out(dx, al);
+    io.out(dx, al);
     // select to write high byte of index
     al = 0xe;
     dx = 0x03d4;
-    out(dx, al);
+    io.out(dx, al);
     // write it
     al = bh;
     dx = 0x03d5;
-    out(dx, al);
+    io.out(dx, al);
     
     dx = 0;
-    ax = 0;
     dl = await getLine();
     imul(dx, 80 * 2);
     di = dx;
     al = await getColumn();
+    ah = 0;
     imul(ax, 2);
     di += ax;
     
     return di;
 }
+
