e86e8373c713ebf7dc039efd12b2506abe8839f5
coderiaser <coderaiser@cloudcmd.io>
2026-02-10 00:46:36 +0200

    chore: nemesis: findFirst

--- externals/nemesis/lib/int/find-file/find-first.ts
+++ externals/nemesis/lib/int/find-file/find-first.ts
@@ -4,40 +4,36 @@ import {nemesis} from "@ishvara/operator-nemesis";
 
 export async function findFirst() {
     push(bx);
-
     cx = 3;
-
     sec_reading_find:
-        al = nemesis.readSector({
-            count: ah,
-            buffer: 0x7c00,
-            sector: 2,
-            track: 0,
-            head: 1,
-        });
+    push(cx);
+    al = nemesis.readSector({
+        count: ah,
+        buffer: 0x7c00,
+        sector: 2,
+        track: 0,
+        head: 1,
+    });
+    pop(cx);
 
-    if (!al)
+    if (al)
         loop(sec_reading_find);
 
     find_sec_loaded:
         si = 0x7c00 - 0x20;
-    cx = 16;              // ← КРИТИЧЕСКИ ВАЖНО
+    
+    pop(cx);
 
     find_file_search:
         si += 0x20;
+    
     lodsb();
-    or(al, al);
-    jz(file_not_found1);
+    
+    if (!al)
+        return 0;
+    
     --si;
     loop(find_file_search);
 
-    find_all_good1:
-        ax = si;
-    pop(bx);
-    ret();
-
-    file_not_found1:
-        ax = 0;
-    pop(bx);
-    ret();
+    return si;
 }
